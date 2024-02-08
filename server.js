8c514c90d145d5545b62fd1284fa85038c069c37
Chad Lou <chadlou@us.ibm.com>
2024-02-08 13:39:57 -0500

    better response

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var app = express();
 var PORT  = process.env.PORT || 8080
 
 var server = app.listen(PORT, function() {
-  var host = server.address().address;
+  var host = require('os').hostname();
   var port = server.address().port;
   console.log('Listening at http://%s:%s', host, port);
 });
 
 
 app.get('/', function (req, res) {
-  res.send('Hello world from Node')
+  res.send('Hello world from Node\n')
 })
 
 app.get('/today', function (req, res) {
@@ -21,7 +21,7 @@ app.get('/today', function (req, res) {
   var month = today.getMonth()+1
   var year = today.getFullYear()
 
-  var todaysDate = `${month}-${day}-${year}`
+  var todaysDate = `${month}-${day}-${year}\n`
 
   res.send(todaysDate)
 })
